71f1459ba8a510332b8be3a56288f64d3dd85575
Olawale <stwalez1@gmail.com>
2022-02-27 17:37:33 +0100

    initial commit

---
+++ server.py
@@ -0,0 +1,33 @@
+from contextlib import redirect_stderr
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+	return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def page(page_name):
+	return render_template(page_name)
+
+def write_to_csv(data):
+	with open('database.csv',  newline='', mode='a') as database2:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		try:
+			data = request.form.to_dict()
+			write_to_csv(data)
+			return redirect('thankyou.html')
+		except Exception as err:
+			print (err)
+			return "could not save to the database"
+	else:
+		return 'something went wrong'
